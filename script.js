ab54f5c4ab8929ed22cb8fa60f3708c6fae36f6f
Adrián Benavidez <>
2023-01-04 14:27:06 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let numIngresado = parseFloat(prompt("ingrese un número"))
+
+if(numIngresado!=10){
+  console.log("El número ingresado es distinto a 10")
+} else {
+  console.log("El número ingresado es 10")
+}
